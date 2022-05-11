5fdc2f71bf39a908a3e0350c653d9fe087360dfd
nrenner <nrenner0211@gmail.com>
2022-05-10 23:06:33 -0400

    saving work

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,19 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 10;
+var playerHealth = 100;
 var playerAttack = 10;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
-    };
 
-console.log(playerName, playerAttack, playerHealth);
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-var enemyName = "Roborto";
-var enemyHealth = 50;
-var enemyAttack = 12;
+    console.log(promptFight);
 
-enemyHealth = enemyHealth - playerAttack;
+    enemyHealth = enemyHealth - playerAttack;
 
-console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
 
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
@@ -26,11 +23,11 @@ console.log(
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-playerHealth = playerHealth - enemyAttack;
+    playerHealth = playerHealth - enemyAttack;
 
-console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
 
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
@@ -38,5 +35,13 @@ console.log(
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
       }
+
+    };
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 fight();
