763b91f6f658d58c1904e26d3b55f7cb4658ecc3
nrenner <nrenner0211@gmail.com>
2022-05-16 23:41:22 -0400

    bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -42,7 +42,8 @@ var fightOrSkip = function() {
 // fight function (now with parameter for enemy's name) 
 var fight = function(enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
-      fightOrSkip(); {
+      //ask player if they'd like to fight or skip
+      if (fightOrSkip()) {
         //if true, leave fight by breaking loop
         break;
       }        
